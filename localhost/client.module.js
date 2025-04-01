05d7faaaf8ed4b37001dcfca39b5f83ca9d6f2ef
jonas.frey@unibe.ch <jonas.frey@unibe.ch>
2025-04-01 12:34:22 +0200

    asdf

--- localhost/client.module.js
+++ localhost/client.module.js
@@ -263,6 +263,90 @@ let f_o_geometry_from_a_o_p_polygon_face = function(a_o_p){
 
 let a_o_function = [
     f_o_function(
+        'its a BOWL!', 
+        function(){
+            
+            let n_its_wave = 12.;
+            // Assuming you have your point generation functions as shown
+            function f_o_vec(x, y, z) {
+                return { n_x: x, n_y: y, n_z: z };
+            }
+    
+            function f_a_o_p(o_trn, n_corners, n_amp, n_rad_offset, n_it_layer_nor) {
+    
+                let a_o = new Array(n_corners).fill(0).map((v, n_idx)=>{
+                    let n_it = parseFloat(n_idx);
+                    let n_it_nor_corner = n_it/n_corners;
+    
+                    let n_x = (n_it_nor_corner*n_its_wave)%1.;
+                    let a = Math.pow(n_x,2);
+                    let b = Math.pow(n_x-1, 2);
+                    let c = Math.min(a,b);
+                    let na = n_amp+c*50.;
+    
+                    let o_trn1 = f_o_vec( //this would be the point that is on the corner of the polygon
+                        Math.sin(n_it_nor_corner*n_tau+n_rad_offset)*na,
+                        Math.cos(n_it_nor_corner*n_tau+n_rad_offset)*na,
+                        0
+                    ); 
+    
+                    o_trn1 = f_o_vec(
+                        o_trn.n_x+o_trn1.n_x,
+                        o_trn.n_y+o_trn1.n_y,
+                        o_trn.n_z+o_trn1.n_z,
+                    )
+                    
+                    return o_trn1
+    
+    
+                }).flat();
+    
+                return a_o
+            }
+    
+            const n_tau = Math.PI * 2;
+            // all units in millimeter mm
+            let n_height = 200.;
+            let n_layer_height = 10.6;
+            let n_its_layer = parseInt(n_height / n_layer_height);
+            let a_o_geometry = []
+            let n_corners = 300.;
+            let a_o_p_outside = [];
+            let n_radius_base = n_height/3;
+            // const phi = (1 + Math.sqrt(5)) / 2;
+    
+    
+            for(let n_it_layer = 0.; n_it_layer < n_its_layer; n_it_layer+=1){
+                let n_it_layer_nor = n_it_layer/n_its_layer;
+                let n_z = n_it_layer*n_layer_height;
+                let n_radius = n_radius_base;
+                let n_expo = 1-Math.pow(n_it_layer_nor-1,2);
+                n_radius += n_expo*100;
+                let n_rad_offset = n_it_layer_nor*(n_tau/n_corners/2);
+                n_rad_offset += n_it_layer_nor*n_tau*(1./n_its_wave);
+                let a_o_p = f_a_o_p(f_o_vec(0, 0, n_z),n_corners,n_radius, n_rad_offset, n_it_layer_nor);
+                a_o_p_outside.push(...a_o_p);
+    
+    
+                if(n_it_layer == 0 || n_it_layer == n_its_layer-1){
+                    // only bottom and top face
+                    a_o_geometry.push(
+                        f_o_geometry_from_a_o_p_polygon_face([f_o_vec(0,0,n_z),...a_o_p])
+                    )
+    
+                }
+            }
+    
+            a_o_geometry.push(
+                // the outside / 'skirt' of the extruded polygon
+                f_o_geometry_from_a_o_p_polygon_vertex(a_o_p_outside, n_corners)
+            )
+            let a_o_mesh = a_o_geometry.map(o=>{return f_o_shaded_mesh(o)})
+            
+            return a_o_mesh
+        }
+    ),
+    f_o_function(
         'example_vase_with_layers', 
         function(){
             // let ov = f_ov({
