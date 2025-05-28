008fb5991533356c3d3369e6d3bd26ef2f17c619
jonas.frey@unibe.ch <jonas.frey@unibe.ch>
2025-05-28 12:52:47 +0200

    stencil

---
+++ localhost/embroidery_stitch_crosstitch_pva_from_json.js
@@ -0,0 +1,862 @@
+
+function(){
+    let o_stitch = {
+        "n_pixels_x": 20,
+        "n_pixels_y": 36,
+        "a_o_pixel_black": [
+            {
+                "n_x": 4,
+                "n_y": 2
+            },
+            {
+                "n_x": 4,
+                "n_y": 3
+            },
+            {
+                "n_x": 4,
+                "n_y": 4
+            },
+            {
+                "n_x": 4,
+                "n_y": 5
+            },
+            {
+                "n_x": 4,
+                "n_y": 6
+            },
+            {
+                "n_x": 4,
+                "n_y": 7
+            },
+            {
+                "n_x": 4,
+                "n_y": 8
+            },
+            {
+                "n_x": 4,
+                "n_y": 16
+            },
+            {
+                "n_x": 4,
+                "n_y": 17
+            },
+            {
+                "n_x": 4,
+                "n_y": 18
+            },
+            {
+                "n_x": 4,
+                "n_y": 19
+            },
+            {
+                "n_x": 4,
+                "n_y": 20
+            },
+            {
+                "n_x": 4,
+                "n_y": 21
+            },
+            {
+                "n_x": 4,
+                "n_y": 22
+            },
+            {
+                "n_x": 5,
+                "n_y": 2
+            },
+            {
+                "n_x": 5,
+                "n_y": 8
+            },
+            {
+                "n_x": 5,
+                "n_y": 9
+            },
+            {
+                "n_x": 5,
+                "n_y": 16
+            },
+            {
+                "n_x": 5,
+                "n_y": 22
+            },
+            {
+                "n_x": 5,
+                "n_y": 23
+            },
+            {
+                "n_x": 6,
+                "n_y": 2
+            },
+            {
+                "n_x": 6,
+                "n_y": 4
+            },
+            {
+                "n_x": 6,
+                "n_y": 5
+            },
+            {
+                "n_x": 6,
+                "n_y": 6
+            },
+            {
+                "n_x": 6,
+                "n_y": 8
+            },
+            {
+                "n_x": 6,
+                "n_y": 9
+            },
+            {
+                "n_x": 6,
+                "n_y": 10
+            },
+            {
+                "n_x": 6,
+                "n_y": 16
+            },
+            {
+                "n_x": 6,
+                "n_y": 18
+            },
+            {
+                "n_x": 6,
+                "n_y": 19
+            },
+            {
+                "n_x": 6,
+                "n_y": 20
+            },
+            {
+                "n_x": 6,
+                "n_y": 22
+            },
+            {
+                "n_x": 6,
+                "n_y": 23
+            },
+            {
+                "n_x": 6,
+                "n_y": 24
+            },
+            {
+                "n_x": 7,
+                "n_y": 2
+            },
+            {
+                "n_x": 7,
+                "n_y": 4
+            },
+            {
+                "n_x": 7,
+                "n_y": 6
+            },
+            {
+                "n_x": 7,
+                "n_y": 8
+            },
+            {
+                "n_x": 7,
+                "n_y": 9
+            },
+            {
+                "n_x": 7,
+                "n_y": 10
+            },
+            {
+                "n_x": 7,
+                "n_y": 11
+            },
+            {
+                "n_x": 7,
+                "n_y": 16
+            },
+            {
+                "n_x": 7,
+                "n_y": 18
+            },
+            {
+                "n_x": 7,
+                "n_y": 20
+            },
+            {
+                "n_x": 7,
+                "n_y": 22
+            },
+            {
+                "n_x": 7,
+                "n_y": 23
+            },
+            {
+                "n_x": 7,
+                "n_y": 24
+            },
+            {
+                "n_x": 7,
+                "n_y": 25
+            },
+            {
+                "n_x": 8,
+                "n_y": 2
+            },
+            {
+                "n_x": 8,
+                "n_y": 6
+            },
+            {
+                "n_x": 8,
+                "n_y": 8
+            },
+            {
+                "n_x": 8,
+                "n_y": 9
+            },
+            {
+                "n_x": 8,
+                "n_y": 10
+            },
+            {
+                "n_x": 8,
+                "n_y": 11
+            },
+            {
+                "n_x": 8,
+                "n_y": 12
+            },
+            {
+                "n_x": 8,
+                "n_y": 16
+            },
+            {
+                "n_x": 8,
+                "n_y": 20
+            },
+            {
+                "n_x": 8,
+                "n_y": 22
+            },
+            {
+                "n_x": 8,
+                "n_y": 23
+            },
+            {
+                "n_x": 8,
+                "n_y": 24
+            },
+            {
+                "n_x": 8,
+                "n_y": 25
+            },
+            {
+                "n_x": 8,
+                "n_y": 26
+            },
+            {
+                "n_x": 9,
+                "n_y": 2
+            },
+            {
+                "n_x": 9,
+                "n_y": 3
+            },
+            {
+                "n_x": 9,
+                "n_y": 4
+            },
+            {
+                "n_x": 9,
+                "n_y": 5
+            },
+            {
+                "n_x": 9,
+                "n_y": 6
+            },
+            {
+                "n_x": 9,
+                "n_y": 8
+            },
+            {
+                "n_x": 9,
+                "n_y": 9
+            },
+            {
+                "n_x": 9,
+                "n_y": 10
+            },
+            {
+                "n_x": 9,
+                "n_y": 11
+            },
+            {
+                "n_x": 9,
+                "n_y": 12
+            },
+            {
+                "n_x": 9,
+                "n_y": 13
+            },
+            {
+                "n_x": 9,
+                "n_y": 16
+            },
+            {
+                "n_x": 9,
+                "n_y": 17
+            },
+            {
+                "n_x": 9,
+                "n_y": 18
+            },
+            {
+                "n_x": 9,
+                "n_y": 19
+            },
+            {
+                "n_x": 9,
+                "n_y": 20
+            },
+            {
+                "n_x": 9,
+                "n_y": 22
+            },
+            {
+                "n_x": 9,
+                "n_y": 23
+            },
+            {
+                "n_x": 9,
+                "n_y": 24
+            },
+            {
+                "n_x": 9,
+                "n_y": 25
+            },
+            {
+                "n_x": 9,
+                "n_y": 26
+            },
+            {
+                "n_x": 9,
+                "n_y": 27
+            },
+            {
+                "n_x": 10,
+                "n_y": 8
+            },
+            {
+                "n_x": 10,
+                "n_y": 9
+            },
+            {
+                "n_x": 10,
+                "n_y": 10
+            },
+            {
+                "n_x": 10,
+                "n_y": 11
+            },
+            {
+                "n_x": 10,
+                "n_y": 12
+            },
+            {
+                "n_x": 10,
+                "n_y": 13
+            },
+            {
+                "n_x": 10,
+                "n_y": 14
+            },
+            {
+                "n_x": 10,
+                "n_y": 22
+            },
+            {
+                "n_x": 10,
+                "n_y": 23
+            },
+            {
+                "n_x": 10,
+                "n_y": 24
+            },
+            {
+                "n_x": 10,
+                "n_y": 25
+            },
+            {
+                "n_x": 10,
+                "n_y": 26
+            },
+            {
+                "n_x": 10,
+                "n_y": 27
+            },
+            {
+                "n_x": 10,
+                "n_y": 28
+            },
+            {
+                "n_x": 11,
+                "n_y": 15
+            },
+            {
+                "n_x": 11,
+                "n_y": 16
+            },
+            {
+                "n_x": 11,
+                "n_y": 17
+            },
+            {
+                "n_x": 11,
+                "n_y": 18
+            },
+            {
+                "n_x": 11,
+                "n_y": 19
+            },
+            {
+                "n_x": 11,
+                "n_y": 20
+            },
+            {
+                "n_x": 11,
+                "n_y": 21
+            },
+            {
+                "n_x": 11,
+                "n_y": 29
+            },
+            {
+                "n_x": 11,
+                "n_y": 30
+            },
+            {
+                "n_x": 11,
+                "n_y": 31
+            },
+            {
+                "n_x": 11,
+                "n_y": 32
+            },
+            {
+                "n_x": 11,
+                "n_y": 33
+            },
+            {
+                "n_x": 11,
+                "n_y": 34
+            },
+            {
+                "n_x": 11,
+                "n_y": 35
+            },
+            {
+                "n_x": 12,
+                "n_y": 9
+            },
+            {
+                "n_x": 12,
+                "n_y": 10
+            },
+            {
+                "n_x": 12,
+                "n_y": 11
+            },
+            {
+                "n_x": 12,
+                "n_y": 12
+            },
+            {
+                "n_x": 12,
+                "n_y": 13
+            },
+            {
+                "n_x": 12,
+                "n_y": 15
+            },
+            {
+                "n_x": 12,
+                "n_y": 16
+            },
+            {
+                "n_x": 12,
+                "n_y": 17
+            },
+            {
+                "n_x": 12,
+                "n_y": 18
+            },
+            {
+                "n_x": 12,
+                "n_y": 19
+            },
+            {
+                "n_x": 12,
+                "n_y": 20
+            },
+            {
+                "n_x": 12,
+                "n_y": 23
+            },
+            {
+                "n_x": 12,
+                "n_y": 24
+            },
+            {
+                "n_x": 12,
+                "n_y": 25
+            },
+            {
+                "n_x": 12,
+                "n_y": 26
+            },
+            {
+                "n_x": 12,
+                "n_y": 27
+            },
+            {
+                "n_x": 12,
+                "n_y": 29
+            },
+            {
+                "n_x": 12,
+                "n_y": 30
+            },
+            {
+                "n_x": 12,
+                "n_y": 31
+            },
+            {
+                "n_x": 12,
+                "n_y": 32
+            },
+            {
+                "n_x": 12,
+                "n_y": 33
+            },
+            {
+                "n_x": 12,
+                "n_y": 34
+            },
+            {
+                "n_x": 13,
+                "n_y": 9
+            },
+            {
+                "n_x": 13,
+                "n_y": 13
+            },
+            {
+                "n_x": 13,
+                "n_y": 15
+            },
+            {
+                "n_x": 13,
+                "n_y": 16
+            },
+            {
+                "n_x": 13,
+                "n_y": 17
+            },
+            {
+                "n_x": 13,
+                "n_y": 18
+            },
+            {
+                "n_x": 13,
+                "n_y": 19
+            },
+            {
+                "n_x": 13,
+                "n_y": 23
+            },
+            {
+                "n_x": 13,
+                "n_y": 27
+            },
+            {
+                "n_x": 13,
+                "n_y": 29
+            },
+            {
+                "n_x": 13,
+                "n_y": 30
+            },
+            {
+                "n_x": 13,
+                "n_y": 31
+            },
+            {
+                "n_x": 13,
+                "n_y": 32
+            },
+            {
+                "n_x": 13,
+                "n_y": 33
+            },
+            {
+                "n_x": 14,
+                "n_y": 9
+            },
+            {
+                "n_x": 14,
+                "n_y": 11
+            },
+            {
+                "n_x": 14,
+                "n_y": 13
+            },
+            {
+                "n_x": 14,
+                "n_y": 15
+            },
+            {
+                "n_x": 14,
+                "n_y": 16
+            },
+            {
+                "n_x": 14,
+                "n_y": 17
+            },
+            {
+                "n_x": 14,
+                "n_y": 18
+            },
+            {
+                "n_x": 14,
+                "n_y": 23
+            },
+            {
+                "n_x": 14,
+                "n_y": 25
+            },
+            {
+                "n_x": 14,
+                "n_y": 27
+            },
+            {
+                "n_x": 14,
+                "n_y": 29
+            },
+            {
+                "n_x": 14,
+                "n_y": 30
+            },
+            {
+                "n_x": 14,
+                "n_y": 31
+            },
+            {
+                "n_x": 14,
+                "n_y": 32
+            },
+            {
+                "n_x": 15,
+                "n_y": 9
+            },
+            {
+                "n_x": 15,
+                "n_y": 11
+            },
+            {
+                "n_x": 15,
+                "n_y": 12
+            },
+            {
+                "n_x": 15,
+                "n_y": 13
+            },
+            {
+                "n_x": 15,
+                "n_y": 15
+            },
+            {
+                "n_x": 15,
+                "n_y": 16
+            },
+            {
+                "n_x": 15,
+                "n_y": 17
+            },
+            {
+                "n_x": 15,
+                "n_y": 23
+            },
+            {
+                "n_x": 15,
+                "n_y": 25
+            },
+            {
+                "n_x": 15,
+                "n_y": 26
+            },
+            {
+                "n_x": 15,
+                "n_y": 27
+            },
+            {
+                "n_x": 15,
+                "n_y": 29
+            },
+            {
+                "n_x": 15,
+                "n_y": 30
+            },
+            {
+                "n_x": 15,
+                "n_y": 31
+            },
+            {
+                "n_x": 16,
+                "n_y": 9
+            },
+            {
+                "n_x": 16,
+                "n_y": 15
+            },
+            {
+                "n_x": 16,
+                "n_y": 16
+            },
+            {
+                "n_x": 16,
+                "n_y": 23
+            },
+            {
+                "n_x": 16,
+                "n_y": 29
+            },
+            {
+                "n_x": 16,
+                "n_y": 30
+            },
+            {
+                "n_x": 17,
+                "n_y": 9
+            },
+            {
+                "n_x": 17,
+                "n_y": 10
+            },
+            {
+                "n_x": 17,
+                "n_y": 11
+            },
+            {
+                "n_x": 17,
+                "n_y": 12
+            },
+            {
+                "n_x": 17,
+                "n_y": 13
+            },
+            {
+                "n_x": 17,
+                "n_y": 14
+            },
+            {
+                "n_x": 17,
+                "n_y": 15
+            },
+            {
+                "n_x": 17,
+                "n_y": 23
+            },
+            {
+                "n_x": 17,
+                "n_y": 24
+            },
+            {
+                "n_x": 17,
+                "n_y": 25
+            },
+            {
+                "n_x": 17,
+                "n_y": 26
+            },
+            {
+                "n_x": 17,
+                "n_y": 27
+            },
+            {
+                "n_x": 17,
+                "n_y": 28
+            },
+            {
+                "n_x": 17,
+                "n_y": 29
+            }
+        ]
+    }; 
+    // Create two meshes
+    let n_extr_base = 0.2;
+    let n_scl_x_pixel = 1
+    let n_scl_y_pixel = 1;
+    let n_scl_margin_pixel_x = n_scl_x_pixel*0.8;
+    let n_scl_margin_pixel_y = n_scl_x_pixel*0.8;
+
+    let n_pixels_x = o_stitch.n_pixels_x;
+    let n_pixels_y = o_stitch.n_pixels_y;
+
+
+    const o_mat_plane = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+
+
+    const o_geo_plane = new THREE.BoxGeometry(
+        n_pixels_x * (n_scl_x_pixel+n_scl_margin_pixel_x)+n_scl_margin_pixel_x*2,
+        n_pixels_y * (n_scl_y_pixel+n_scl_margin_pixel_y)+n_scl_margin_pixel_y*2,
+        n_extr_base
+    );
+
+    // let o_mesh_plane = f_o_shaded_mesh(o_geo_plane);
+    let o_mesh_plane = new THREE.Mesh(o_geo_plane, o_mat_plane);
+    let o_mesh_plane_original = o_mesh_plane.clone();
+
+    let a_o_mesh_pixel = [];
+    
+
+    let o_box_csg_plane = o_mod_csg.CSG.fromMesh(o_mesh_plane);
+
+    let n_trn_x = -n_pixels_x * (n_scl_x_pixel+n_scl_margin_pixel_x)/2;
+    let n_trn_y = -n_pixels_y * (n_scl_y_pixel+n_scl_margin_pixel_y)/2;
+    for(let n_it_x = 0; n_it_x < n_pixels_x; n_it_x++){
+        for(let n_it_y = 0; n_it_y < n_pixels_y; n_it_y++){
+
+            let o_pixel = o_stitch.a_o_pixel_black.find(
+                o=>{
+                    return o.n_x == n_it_x && o.n_y == n_it_y
+                }
+            );
+            if(!o_pixel){
+                continue
+            }
+            let o_geo_pixel = new THREE.BoxGeometry(
+                n_scl_x_pixel, 
+                n_scl_y_pixel, 
+                n_extr_base*10
+            );
+            
+            let o_mesh_pixel = new THREE.Mesh(o_geo_pixel, o_mat_plane);//f_o_shaded_mesh(o_geo_pixel);
+     
+            // a_o_mesh_pixel.push(o_mesh_pixel);
+            o_mesh_pixel.position.x = n_trn_x+(n_it_x * (n_scl_x_pixel+n_scl_margin_pixel_x));
+            o_mesh_pixel.position.y = n_trn_y+(n_it_y * (n_scl_y_pixel+n_scl_margin_pixel_y));
+            o_mesh_pixel.position.z = -n_extr_base/2;
+            o_mesh_pixel.updateMatrix(); // This is crucial!
+            
+            const o_csg_pixel = o_mod_csg.CSG.fromMesh(o_mesh_pixel);
+            o_box_csg_plane = o_box_csg_plane.subtract(o_csg_pixel);  // Subtract
+
+
+        }
+    }
+    o_mesh_plane = o_mod_csg.CSG.toMesh(o_box_csg_plane, o_mesh_plane.matrix, o_mat_plane);
+
+    // const resultMesh = o_mod_csg.CSG.toMesh(o_box_csg_plane, o_mesh_plane.matrix, o_mat_plane);
+    return [
+        o_mesh_plane, 
+        // ...a_o_mesh_pixel
+    ];
+}
